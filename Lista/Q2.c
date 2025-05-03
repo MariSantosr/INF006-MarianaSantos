4f1e80272cfe14e2b3eb2f577aee6a5f68f8a051
Mariana Santos <144575293+MariSantosr@users.noreply.github.com>
2025-05-03 19:29:15 +0000

    finalizada sem teste

--- Lista/Q2.c
+++ Lista/Q2.c
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <math.h>
 
-#define LINE_MAX 1024
+#define LINHA_MAX 1024
 #define TOKEN_MAX 255
 
 typedef struct {
@@ -17,7 +17,7 @@ typedef struct {
 } Pontos;
 
 //Função para identificar Pontos
-int ponto (const char *str, double *x, double *y, char *txt_original){
+int str_ponto (const char *str, double *x, double *y, char *txt_original){
     char temp[64];
     char *virgula, *xstr, *ystr, *end1, *end2;
     size_t len = strlen(str);
@@ -87,7 +87,7 @@ int comparar_string(const void *a, const void *b) {
 }
 
 int main (){
-
+//Chamando os arquivos de entrada e saída
   FILE *fp_in = fopen("L0Q2.in", "r");
   FILE *fp_out = fopen("L0Q2.out", "w");
 
@@ -96,9 +96,75 @@ int main (){
       return 1;
   } 
 
+  char linha[LINHA_MAX];
+  char *tokens[TOKEN_MAX];
+  int qtde_tokens = 0;
 
+    while (fgets(linha, sizeof(linha), fp_in)) {
+        linha[strcspn(linha, "\n")] = '\0'; 
 
-  
+        char *token = strtok(linha, " ");
+        while (token) {
+            tokens[qtde_tokens++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        char *strings[TOKEN_MAX];
+        int ints[TOKEN_MAX];
+        int num_inteiro = 0;
+        int num_string = 0;
+        int num_float = 0;
+        int num_pontos = 0;
+        double floats[TOKEN_MAX];
+        Pontos points[TOKEN_MAX];
+
+        for (int i = 0; i < qtde_tokens; i++) {
+            double x, y;
+            char original[64];
+            if (str_ponto(tokens[i], &x, &y, original)) {
+                points[num_pontos].x = x;
+                points[num_pontos].y = y;
+                points[num_pontos].distancia = sqrt(x * x + y * y);
+                strcpy(points[num_pontos].txt_original, original);
+                num_pontos++;
+            } else if (str_inteiro(tokens[i])) {
+                ints[num_inteiro++] = atoi(tokens[i]);
+            } else if (str_float(tokens[i])) {
+                floats[num_float++] = atof(tokens[i]);
+            } else {
+                strings[num_string++] = tokens[i];
+            }
+        }
+
+        qsort(strings, num_string, sizeof(char *), comparar_string);
+        qsort(ints, num_inteiro, sizeof(int), comparar_inteiros);
+        qsort(floats, num_float, sizeof(double), comparar_double);
+        qsort(points, num_pontos, sizeof(Pontos), comparar_pontos);
+
+        fprintf(fp_out, "str:");
+        for (int i = 0; i < num_string; i++) {
+            fprintf(fp_out, "%s%s", (i > 0 ? " " : ""), strings[i]);
+        }
+
+        fprintf(fp_out, " int:");
+        for (int i = 0; i < num_inteiro; i++) {
+            fprintf(fp_out, "%s%d", (i > 0 ? " " : ""), ints[i]);
+        }
+
+        fprintf(fp_out, " float:");
+        for (int i = 0; i < num_float; i++) {
+            fprintf(fp_out, "%s%.15g", (i > 0 ? " " : ""), floats[i]);
+        }
+
+        fprintf(fp_out, " p:");
+        for (int i = 0; i < num_pontos; i++) {
+            fprintf(fp_out, "%s%s", (i > 0 ? " " : ""), points[i].txt_original);
+        }
+
+        fprintf(fp_out, "\n");
+
+    }//fim do while
+//Fechando os arquivos de entrada e saída
 fclose(fp_in);
 fclose(fp_out);
 
