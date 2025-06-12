99f6f97329047b7919f5301d5389b94386411bc0
Mariana Santos <144575293+MariSantosr@users.noreply.github.com>
2025-06-12 18:44:13 +0000

    Q2 criada

---
+++ Lista2/Q2.c
@@ -0,0 +1,16 @@
+//Dupla: Mariana Santos (20242160028) e Thais Menezes (20242160013)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+
+//main principal
+int main () {
+
+//chamando os arquivos de entrada/saida
+
+
+
+    return 0;
+}
