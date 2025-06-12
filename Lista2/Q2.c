e8e43ac57116f4035bb202e4d3501bbec349bee6
Mariana Santos <144575293+MariSantosr@users.noreply.github.com>
2025-06-12 22:48:06 +0000

    struct pilha criado

--- Lista2/Q2.c
+++ Lista2/Q2.c
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 #include <string.h> 
 
+#define LINHA_MAX 3001 // MAX_LINE
+#define TAM_NOMES_MAX 100 // MAX_NAME_LEN
+#define PILHAS_NOMES_MAX 100 // MAX_NAMES_STACK
+#define TAM_BUFFER_SAIDA_MAX (PILHAS_NOMES_MAX * (TAM_NOMES_MAX + 6 + 1) + PILHAS_NOMES_MAX * 10)
+
+//Struct base da pilha 
+typedef struct {
+    char nomes[PILHAS_NOMES_MAX][TAM_NOMES_MAX];
+    int topoPilha;
+}Pilha;
 
 //main principal
 int main () {
